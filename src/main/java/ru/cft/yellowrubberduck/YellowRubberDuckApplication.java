852be71459856fa74ebcbdc090226975c2aa3cc5
Sevo98 <sevo98.sk@gmail.com>
2022-02-02 00:44:08 +0700

    1. Test commit

--- src/main/java/ru/cft/yellowrubberduck/YellowRubberDuckApplication.java
+++ src/main/java/ru/cft/yellowrubberduck/YellowRubberDuckApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YellowRubberDuckApplication {
-//Test commit
+//Test commitеее
 	public static void main(String[] args) {
 		SpringApplication.run(YellowRubberDuckApplication.class, args);
 	}
