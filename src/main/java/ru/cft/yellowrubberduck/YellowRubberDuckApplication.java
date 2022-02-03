3d76afdd0741a5de18953fc7899805115330ae19
Stanislav Grokhotov <stanislav.skii2000@mail.ru>
2022-02-03 23:12:30 +0700

    added empty class LicenseService

--- src/main/java/ru/cft/yellowrubberduck/YellowRubberDuckApplication.java
+++ src/main/java/ru/cft/yellowrubberduck/YellowRubberDuckApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class YellowRubberDuckApplication {
-//Test commitеее
+
 	public static void main(String[] args) {
 		SpringApplication.run(YellowRubberDuckApplication.class, args);
 	}
